92cb3e3052344f5dfeddb6f4074bd7b657fce958
hulkhaugen <borre.haugen@gmail.com>
2023-02-25 10:45:28 +0100

    Merged morningstar and morningstar_lt
    Merged the two sensors together as they were already very similar

--- morningstar/sensor.py
+++ morningstar/sensor.py
@@ -1,94 +1,150 @@
 """Data from Morningstar"""
 import asyncio
-from datetime import timedelta
 import logging
+from datetime import timedelta
 
 import aiohttp
 import async_timeout
-from bs4 import BeautifulSoup
 import voluptuous as vol
+from bs4 import BeautifulSoup
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (ATTR_ATTRIBUTION, CONF_CURRENCY, CONF_SCAN_INTERVAL)
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import ATTR_ATTRIBUTION, CONF_CURRENCY, CONF_SCAN_INTERVAL
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTRIBUTION = 'Data provided by Morningstar'
-CONF_FUNDS = 'funds'
-DEFAULT_CURRENCY = 'kr'
+ATTRIBUTION = "Data provided by Morningstar"
+CONF_FUNDS = "funds"
+CONF_LT_FUNDS = "lt_funds"
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=30)
-DOMAIN = 'morningstar'
-URL = 'https://www.morningstar.no/no/funds/snapshot/snapshot.aspx?id={}'
+DOMAIN = "morningstar"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_FUNDS): vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_CURRENCY, default=DEFAULT_CURRENCY): cv.string,
-        vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period
+        # Figure out a way to require only one, but both can be used simultanoiusly
+        vol.Optional(CONF_FUNDS): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_LT_FUNDS): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_CURRENCY, default="0"): cv.string,
+        vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period,
     }
 )
 
 
-async def async_scape(sess, fund):
+async def async_scrape(session: object, fund: str, url: str) -> BeautifulSoup:
+    """Download data from Morningstar and parse it with BeautifulSoup."""
     try:
         with async_timeout.timeout(10):
-            response = await sess.get(URL.format(fund))
-        _LOGGER.info('Response from Morningstar fund %s: %s', fund, response.status)
-        html = await response.text()
-        soup = BeautifulSoup(html, 'html.parser')
-        try:
-            keys = soup.find('table', attrs={'class': 'overviewKeyStatsTable'})
-            hist = soup.find('table', attrs={'class': 'overviewTrailingReturnsTable'})
-            name = soup.h1.text
-            stat = float(keys.select('tr:nth-of-type(2) > td.text')[0].text[4:].replace(',', '.'))
-            oday = float(keys.select('tr:nth-of-type(3) > td.text')[0].text.replace(',', '.').replace('%\n', ''))
-            icon = 'mdi:trending-up' if oday > 0 else 'mdi:trending-down' if oday < 0 else 'mdi:trending-neutral'
-            attr = {
-                ATTR_ATTRIBUTION: ATTRIBUTION,
-                'Dato': hist.find('td', attrs={'class': 'heading date'}).text,
-                '1 dag': str(oday).replace('.', ',') + ' %'
-            }
-            hist = [[td.text for td in tr.select('td')] for tr in hist.select('tr')][1:]
-            hist = {item[0]: item[1] + ' %' for item in hist if item[1] != '-'}
-            attr.update(hist)
-            attr['URL'] = URL.format(fund)
-            data = {'name': name, 'stat': stat, 'icon': icon, 'attr': attr}
-            _LOGGER.info('%s successfully scraped from Morningstar', name)
-            return data
-        except (IndexError, AttributeError):
-            _LOGGER.warning('Unable to extract data from Morningstar for %s', fund)
-            return
+            response = await session.get(url.format(fund))
     except (asyncio.TimeoutError, aiohttp.ClientError):
-        _LOGGER.info('Unable to scrape data from Morningstar for %s', fund)
-        return
+        _LOGGER.warning("Unable to scrape data for %s", fund)
+        return None
+    html = await response.text()
+    _LOGGER.info("Response from Morningstar for %s: %s", fund, response.status)
+    return BeautifulSoup(html, "html.parser")
+
+
+async def async_morningstar(session: object, fund: str) -> dict:
+    """Initiate the scraper and process the data."""
+    url = "https://www.morningstar.no/no/funds/snapshot/snapshot.aspx?id={}"
+    try:
+        soup = await async_scrape(session, fund, url)
+        name = soup.h1.text
+        keys = soup.find("table", attrs={"class": "overviewKeyStatsTable"})
+        hist = soup.find("table", attrs={"class": "overviewTrailingReturnsTable"})
+        stat = keys.select("tr:nth-of-type(2) > td.text")[0].text[4:].replace(",", ".")
+        unit = keys.select("tr:nth-of-type(2) > td.text")[0].text[:3]
+        date = hist.find("td", attrs={"class": "heading date"}).text
+        oday = float(
+            keys.select("tr:nth-of-type(3) > td.text")[0]
+            .text.replace(",", ".")
+            .replace("%\n", "")
+        )
+        icon = (
+            "mdi:trending-up"
+            if oday > 0
+            else "mdi:trending-down"
+            if oday < 0
+            else "mdi:trending-neutral"
+        )
+        attr = {ATTR_ATTRIBUTION: ATTRIBUTION, "Dato": date, "1 dag": f"{oday:.2f} %"}
+        hist = [[td.text for td in tr.select("td")] for tr in hist.select("tr")][1:]
+        hist = {
+            item[0]: f"{float(item[1].replace(',', '.')):.2f} %"
+            for item in hist
+            if item[1] != "-"
+        }
+        attr.update(hist)
+        attr["URL"] = url.format(fund)
+        _LOGGER.info("%s successfully scraped from Morningstar", name)
+        return {"name": name, "stat": stat, "unit": unit, "icon": icon, "attr": attr}
+    except AttributeError:
+        _LOGGER.warning("Unable to extract data from Morningstar for %s", fund)
+        return None
+
+
+async def async_morningstar_lt(session: object, fund: str) -> dict:
+    """Initiate the scraper and process the data."""
+    url = "https://lt.morningstar.com/cahq7idbwv/snapshot/snapshot.aspx?id={}"
+    try:
+        soup = await async_scrape(session, fund, url)
+        name = soup.h1.text
+        stat = soup.select("#KeyStatsLatestNav td")[0].text[4:].replace(",", ".")
+        unit = soup.select("#KeyStatsLatestNav td")[0].text[:3]
+        pcts = soup.select("#TrailingReturns > table > tbody > tr > td.colSecurity")
+        span = soup.select("#TrailingReturns > table > tbody > tr > th")
+        date = soup.select("#KeyStatsLatestNav > th > span")[0].text
+        oday = float(pcts[1].text.replace(",", "."))
+        icon = (
+            "mdi:trending-up"
+            if oday > 0
+            else "mdi:trending-down"
+            if oday < 0
+            else "mdi:trending-neutral"
+        )
+        attr = {ATTR_ATTRIBUTION: ATTRIBUTION, "Dato": date}
+        hist = {span[i].text: pcts[i].text + " %" for i in range(len(pcts))}
+        attr.update(hist)
+        attr["URL"] = url.format(fund)
+        _LOGGER.info("%s successfully scraped from Morningstar LT", name)
+        return {"name": name, "stat": stat, "unit": unit, "icon": icon, "attr": attr}
+    except (IndexError, AttributeError):
+        _LOGGER.warning("Unable to extract data from Morningstar LT for %s", fund)
+        return None
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the sensor."""
-    _LOGGER.info('Setting up sensors')
-    sess = async_get_clientsession(hass)
-    funds = config.get(CONF_FUNDS, [])
+    """Set up the Morningstar sensor."""
+    _LOGGER.info("Setting up sensors")
+    session = async_get_clientsession(hass)
+    funds = config[CONF_FUNDS]
+    lt_funds = config[CONF_LT_FUNDS]
     unit = config.get(CONF_CURRENCY)
+    entities = []
     for fund in funds:
-        async_add_entities([MorningstarSensor(sess, fund, unit)], True)
-        _LOGGER.info('Setup of %s complete', fund)
+        data = await async_morningstar(session, fund)
+        if data:
+            entities.append(MorningstarSensor(data, fund, unit, True))
+    for lt_fund in lt_funds:
+        data = await async_morningstar_lt(session, lt_fund)
+        if data:
+            entities.append(MorningstarSensor(data, lt_fund, unit, False))
+    async_add_entities(entities)
 
 
 class MorningstarSensor(Entity):
-    """Representation of the sensor."""
-    def __init__(self, sess, fund, unit):
-        """Initialize the sensor."""
-        self._sess = sess
+    """Initiate the scraper and process the data."""
+
+    def __init__(self, data, fund, unit, morn):
+        self._attr = data["attr"]
+        self._icon = data["icon"]
+        self._name = data["name"]
+        self._stat = data["stat"]
+        self._unit = data["unit"] if unit == "0" else unit
         self._fund = fund
-        self._unit = unit
-        self._data = None
-        self._name = None
-        self._stat = None
-        self._icon = None
-        self._attr = None
+        self._morn = morn
 
     @property
     def name(self):
@@ -97,37 +153,40 @@ class MorningstarSensor(Entity):
 
     @property
     def unique_id(self):
-        """Return the unique ID."""
+        """Return the unique ID for the sensor."""
         return self._fund
 
     @property
     def state(self):
-        """Return the state of the sensor."""
+        """Return sensor value."""
         return self._stat
 
     @property
     def unit_of_measurement(self):
-        """Return the unit of measurement."""
+        """Return the currency for the sensor."""
         return self._unit
 
     @property
     def icon(self):
-        """Return icon for the sensor."""
+        """Return the sensor icon."""
         return self._icon
 
     @property
     def extra_state_attributes(self):
-        """Return the extra state attributes of the sensor."""
+        """Return the sensor attributes."""
         return self._attr
 
     async def async_update(self):
-        """Update the sensor data."""
-        self._data = await async_scape(self._sess, self._fund)
-        try:
-            self._name = self._data['name']
-            self._stat = self._data['stat']
-            self._icon = self._data['icon']
-            self._attr = self._data['attr']
-            _LOGGER.info('Update of %s complete', self._name)
-        except TypeError:
-            _LOGGER.info('Update failed')
+        """Update sensor data."""
+        session = async_get_clientsession(self.hass)
+        if self._morn:
+            data = await async_morningstar(session, self._fund)
+        else:
+            data = await async_morningstar_lt(session, self._fund)
+        if data:
+            self._attr = data["attr"]
+            self._icon = data["icon"]
+            self._name = data["name"]
+            self._stat = data["stat"]
+        else:
+            _LOGGER.warning("Failed to update %s", self._name)
